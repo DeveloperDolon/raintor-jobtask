9c2084357c9a194b4cd89c3efc808cfdcdb4f26a
DeveloperDolon <dolonr718@gmail.com>
2025-06-20 13:55:30 +0600

    theme mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
+import bgImage from "./assets/hero-bg.png";
+import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <main>
-      <header>
+      <header
+        className="min-h-screen"
+        style={{
+          background: `url(${bgImage}) no-repeat top center`,
+          backgroundSize: "cover",
+        }}
+      >
         <Navbar />
-        
+        <Hero />
       </header>
     </main>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+        <h1 className="">Trusted <span className="bg-black text-white">Partner</span></h1>
+    </div>
+  );
+};
+
+export default Hero;
